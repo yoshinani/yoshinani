9103237b7ec88f195fd1e00eb5d000da7e15f3c9
Yuta Nakano <yutanakano395@icloud.com>
2017-10-27 02:05:35 +0900

    Add client information on twitter and facebook

--- config/services.php
+++ config/services.php
@@ -35,6 +35,18 @@ return [
         'secret' => env('STRIPE_SECRET'),
     ],
 
+    'facebook' => [
+        'client_id'     => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect'      => env('FACEBOOK_CALLBACK_URL'),
+    ],
+
+    'twitter' => [
+        'client_id'     => env('TWITTER_CLIENT_ID'),
+        'client_secret' => env('TWITTER_CLIENT_SECRET'),
+        'redirect'      => env('TWITTER_CALLBACK_URL'),
+    ],
+
     'github' => [
         'client_id'     => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
