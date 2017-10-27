18f2abc78ee12f0a69b5a40734a2ffd5626590e8
Yuta Nakano <yutanakano395@icloud.com>
2017-10-28 03:54:20 +0900

    Fix Change the path of SocialService

--- app/Http/Controllers/Auth/SocialController.php
+++ app/Http/Controllers/Auth/SocialController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Laravel\Socialite\Facades\Socialite;
-use App\Services\SocialService;
+use Domain\Services\Auth\SocialService;
 
 class SocialController extends Controller
 {
