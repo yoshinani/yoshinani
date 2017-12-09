4fc64d7a0d3a2bf375f0997d112dd20f52ea86f2
Yuta Nakano <yutanakano395@icloud.com>
2017-12-09 19:53:21 +0900

    Remove unnecessary code

--- app/Http/Controllers/Auth/SocialController.php
+++ app/Http/Controllers/Auth/SocialController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Auth;
 use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Http\RedirectResponse;
 use App\Http\Controllers\Controller;
-use App\Services\Auth\SocialService as SocialAppService;
 use Domain\Services\AuthService as AuthDomainService;
 use Exception;
 
@@ -15,19 +14,15 @@ use Exception;
  */
 class SocialController extends Controller
 {
-    private $socialAppService;
     private $authDomainService;
 
     /**
      * SocialController constructor.
-     * @param SocialAppService $socialAppService
      * @param AuthDomainService $authDomainService
      */
     public function __construct(
-        SocialAppService $socialAppService,
         AuthDomainService $authDomainService
     ) {
-        $this->socialAppService = $socialAppService;
         $this->authDomainService = $authDomainService;
     }
 
@@ -53,7 +48,7 @@ class SocialController extends Controller
             return redirect('/login');
         }
 
-        $this->socialAppService->existsItems($socialUser);
+        $this->authDomainService->socialRegisterUser($socialUser);
         $this->authDomainService->socialLogin($socialServiceName, $socialUser);
 
         return redirect()->to('/home');
--- domain/Services/AuthService.php
+++ domain/Services/AuthService.php
@@ -3,6 +3,7 @@
 namespace Domain\Services;
 
 use Auth;
+use Domain\Entities\SocialUserAccountEntity;
 use Exception;
 use Infrastructure\Interfaces\SocialRepositoryInterface;
 use Laravel\Socialite\Contracts\User as SocialUser;
@@ -26,24 +27,31 @@ class AuthService
     }
 
     /**
+     * @param SocialUser $socialUser
+     * @throws Exception
+     */
+    public function socialRegisterUser(SocialUser $socialUser)
+    {
+        $hasEmail = $this->hasSocialEmail($socialUser);
+        $hasUserName = $this->hasSocialUserName($socialUser);
+        if ($hasEmail === false && $hasUserName === false) {
+            throw new Exception('Name or Email is missing');
+        }
+
+        $userEntity = $this->socialRepository->findUser($socialUser);
+        if (is_null($userEntity)) {
+            $this->socialRepository->registerUser($socialUser);
+        }
+    }
+
+    /**
      * @param string $socialServiceName
      * @param SocialUser $socialUser
      * @throws Exception
      */
     public function socialLogin(string $socialServiceName, SocialUser $socialUser)
     {
-        $userEntity = $this->socialRepository->findUser($socialUser);
-        if (is_null($userEntity)) {
-            $this->socialRepository->registerUser($socialUser);
-        }
-
-        $userId = $this->socialRepository->getUserId($socialUser);
-        $socialUserAccountEntity = $this->socialRepository->findSocialAccount($userId, $socialServiceName, $socialUser);
-        if (is_null($socialUserAccountEntity)) {
-            $userId = $this->socialRepository->getUserId($socialUser);
-            $this->socialRepository->associationSocialAccount($userId, $socialServiceName, $socialUser);
-            $socialUserAccountEntity = $this->socialRepository->findSocialAccount($userId, $socialServiceName, $socialUser);
-        }
+        $socialUserAccountEntity = $this->synchronizeSocialAccount($socialServiceName, $socialUser);
 
         if (!$socialUserAccountEntity->getSocialServiceName() === $socialServiceName) {
             throw new Exception('Authentication drivers do not match');
@@ -57,4 +65,46 @@ class AuthService
             throw new Exception('It is a User that does not exist');
         }
     }
+
+    /**
+     * @param SocialUser $socialUser
+     * @return bool
+     */
+    protected function hasSocialEmail(SocialUser $socialUser): bool
+    {
+        if (!is_null($socialUser->getEmail())) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @param SocialUser $socialUser
+     * @return bool
+     */
+    protected function hasSocialUserName(SocialUser $socialUser): bool
+    {
+        if (!is_null($socialUser->getName())) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @param string $socialServiceName
+     * @param SocialUser $socialUser
+     * @return SocialUserAccountEntity
+     */
+    protected function synchronizeSocialAccount(string $socialServiceName, SocialUser $socialUser): SocialUserAccountEntity
+    {
+        $userId = $this->socialRepository->getUserId($socialUser);
+        $socialUserAccountEntity = $this->socialRepository->findSocialAccount($userId, $socialServiceName, $socialUser);
+        if (is_null($socialUserAccountEntity)) {
+            $userId = $this->socialRepository->getUserId($socialUser);
+            $this->socialRepository->associationSocialAccount($userId, $socialServiceName, $socialUser);
+            $socialUserAccountEntity = $this->socialRepository->findSocialAccount($userId, $socialServiceName, $socialUser);
+        }
+        return $socialUserAccountEntity;
+    }
+
 }
