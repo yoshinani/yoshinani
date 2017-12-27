2ee2f28c156c8abdf995c7884ad428fa24d1a964
Yuta Nakano <yutanakano395@icloud.com>
2017-12-27 16:50:30 +0900

    Add LogMessage

--- domain/Services/AuthService.php
+++ domain/Services/AuthService.php
@@ -108,23 +108,37 @@ class AuthService
     /**
      * @param string $socialServiceName
      * @param SocialUser $socialUser
-     * @throws Exception
+     * @return bool
      */
-    public function socialLogin(string $socialServiceName, SocialUser $socialUser)
+    public function socialLogin(string $socialServiceName, SocialUser $socialUser): bool
     {
         $socialUserAccountEntity = $this->synchronizeSocialAccount($socialServiceName, $socialUser);
 
-        if (!$socialUserAccountEntity->getSocialServiceName() === $socialServiceName) {
-            throw new Exception('Authentication drivers do not match');
+        if ($socialUserAccountEntity->getSocialServiceName() === $socialServiceName) {
+            \Log::info("\n【ERROR】Authentication drivers do not match\n"
+                .'Entity:'.$socialUserAccountEntity->getSocialServiceName().':'.$socialUserAccountEntity->getSocialUserId()."\n"
+                .'Request:'.$socialServiceName.':'.$socialUser->getId()
+            );
+            return false;
         }
 
         if (!$socialUserAccountEntity->getSocialUserId() === $socialUser->getId()) {
-            throw new Exception('It does not match the ID of SNSAccount');
+            \Log::info("\n【ERROR】It does not match the ID of SNS Account\n"
+                .'Entity:'.$socialUserAccountEntity->getSocialServiceName().':'.$socialUserAccountEntity->getSocialUserId()."\n"
+                .'Request:'.$socialServiceName.':'.$socialUser->getId()
+            );
+            return false;
         }
 
         if (!Auth::loginUsingId($socialUserAccountEntity->getId())) {
-            throw new Exception('It is a User that does not exist');
+            \Log::info("【ERROR】It is a User that does not exist\n"
+                .'Entity:'.$socialUserAccountEntity->getSocialServiceName().':'.$socialUserAccountEntity->getSocialUserId()."\n"
+                .'Request:'.$socialServiceName.':'.$socialUser->getId()
+            );
+            return false;
         }
+
+        return true;
     }
 
     /**
