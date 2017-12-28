0aef4aa843f32b4ec7f1744d0b1cdf9fb2135745
Yuta Nakano <yutanakano395@icloud.com>
2017-12-28 13:29:00 +0900

    Change to equivalent comparison

--- domain/Services/AuthService.php
+++ domain/Services/AuthService.php
@@ -119,7 +119,7 @@ class AuthService
     {
         $socialUserAccountEntity = $this->synchronizeSocialAccount($socialServiceName, $socialUser);
 
-        if ($socialUserAccountEntity->getSocialServiceName() !== $socialServiceName) {
+        if ($socialUserAccountEntity->getSocialServiceName() != $socialServiceName) {
             \Log::info("\n【ERROR】Authentication drivers do not match\n"
                 .'Entity:'.$socialUserAccountEntity->getSocialServiceName().':'.$socialUserAccountEntity->getSocialUserId()."\n"
                 .'Request:'.$socialServiceName.':'.$socialUser->getId()
@@ -127,7 +127,7 @@ class AuthService
             return false;
         }
 
-        if ($socialUserAccountEntity->getSocialUserId() !== $socialUser->getId()) {
+        if ($socialUserAccountEntity->getSocialUserId() != $socialUser->getId()) {
             \Log::info("\n【ERROR】It does not match the ID of SNS Account\n"
                 .'Entity:'.$socialUserAccountEntity->getSocialServiceName().':'.$socialUserAccountEntity->getSocialUserId()."\n"
                 .'Request:'.$socialServiceName.':'.$socialUser->getId()
