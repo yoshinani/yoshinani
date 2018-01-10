bbc5906447471006cb0128ab6fe5c17c64ece249
raichel <yutanakano395@icloud.com>
2018-01-10 22:09:07 +0900

    TimeStampValueObjectTestを追加 (#110)
    * Add TimeStampValueObjectTest
    
    * Remove unnecessary code

---
+++ tests/Unit/ValueObjects/TimeStampValueObjectTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit;
+
+use Domain\ValueObjects\TimeStampValueObject;
+use Tests\TestCase;
+
+class TimeStampValueObjectTest extends TestCase
+{
+    private $timeStamp;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->timeStamp = new TimeStampValueObject();
+    }
+
+    /**
+     * Current date and time formats match
+     * 現在の日付と時刻の形式が一致する
+     * @test
+     */
+    public function getNowFormat()
+    {
+        $this->assertEquals($this->timeStamp->getNow(), date('Y-m-d H:i:s'));
+    }
+
+}
