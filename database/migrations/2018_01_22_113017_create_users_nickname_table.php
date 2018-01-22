d0f6c05bfed73d0fabd18d3fb7e87f17e7946557
Yuta Nakano <yutanakano395@icloud.com>
2018-01-22 11:31:53 +0900

    Add users_nickname_table

---
+++ database/migrations/2018_01_22_113017_create_users_nickname_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersNicknameTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_nickname', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('user_id');
+            $table->string('nickname')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_nickname');
+    }
+}
