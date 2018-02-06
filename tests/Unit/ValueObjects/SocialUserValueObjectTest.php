8ca1ef9b6cc1f521b948bb7c4d87f45aa9485bba
Yuta Nakano <yutanakano395@icloud.com>
2018-02-07 00:33:12 +0900

    Add SocialUserValueObjectTest

---
+++ tests/Unit/ValueObjects/SocialUserValueObjectTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Unit;
+
+use Domain\ValueObjects\SocialUserValueObject;
+use Laravel\Socialite\One\User;
+use Tests\TestCase;
+
+class SocialUserValueObjectTest extends TestCase
+{
+    private $user;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $driverName = 'driverName';
+        $socialUser = new User();
+        $socialUser->id = 1;
+        $socialUser->nickname = "testName";
+        $socialUser->name = "testName";
+        $socialUser->email = "test@test.test";
+        $this->user = new SocialUserValueObject($driverName, $socialUser);
+    }
+
+    /**
+     * Matches the acquired DriverName.
+     * 取得したドライバー名と一致します。
+     * @test
+     */
+    public function getDriverNameMatch()
+    {
+        $this->assertEquals($this->user->getDriverName() , 'driverName');
+    }
+
+    /**
+     * Matches the acquired SocialUserId.
+     * 取得したソーシャルユーザIDに一致します。
+     * @test
+     */
+    public function getSocialUserIdMatch()
+    {
+        $this->assertEquals($this->user->getSocialUserId(), 1);
+    }
+}
