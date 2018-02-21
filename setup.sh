d0b8d842ed04eececc53ba697b3a4fa8575b5b38
Yuta Nakano <yutanakano395@icloud.com>
2018-02-22 01:14:55 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,32 +1,13 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -eu
+
 cd `dirname $0`
-path=$(pwd)
-homestead=$HOME/homestead
-
-# Generate when Homestead.yaml does not exist
-if [ ! -e ${path}/Homestead.yaml ]; then
-  cp ${path}/Homestead/Homestead.yaml ${path}/Homestead.yaml
-  sed -i '' "s:ApplicationPath:${path}:g" ${path}/Homestead.yaml
-fi
-
-# When Homestead.yaml does not exist
-if [ ! -e ${homestead} ]; then
-  # When the argument does not exist
-  if [ "$1" = "" ]; then
-    echo 'Absolute path is required for argument'
-    exit
-  fi
-  # When Homestead does not exist
-  if [ ! -e $1 ]; then
-    echo 'Homestaead does not exist'
-    exit
-  fi
-  ${homestead}=$1
-fi
-
-# Generate when Homestead.yaml does not exist
-if [ ! -e ${homestead}/Homestead.yaml ]; then
-  cp ${path}/Homestead.yaml ${homestead}
-fi
-
-echo 'Setup succeeded'
+cp .env.example .env
+# Install composer
+composer install
+# Generate key
+php artisan key:generate
+# Create ide-helper
+php artisan ide-helper:generate 1>/dev/null 2>/dev/null
+# Run migration
+php artisan migrate 1>/dev/null 2>/dev/null
+echo "Application setting succeeded"
