56bafb018ebe0a8e5bfc46b0753b9bddb823412f
Yuta Nakano <yutanakano395@icloud.com>
2018-07-09 02:22:08 +0900

    Add deleted_users_table

---
+++ database/migrations/2018_07_01_153137_create_deleted_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDeletedUsersTable extends Migration
+{
+    private $table = 'deleted_users';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->table, function (Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('user_id')->unique();
+            $table->string('name');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists($this->table);
+    }
+}
